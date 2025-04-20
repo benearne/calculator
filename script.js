4fe789a910225ebb04df39cfa07d0da51915171d
benearne <b.arnold@posteo.de>
2025-04-20 14:55:53 +0200

    start with functions in script

--- script.js
+++ script.js
@@ -10,4 +10,4 @@ function multiply()
 function divide()
 
 // Create a new function operate that takes an operator and two numbers and then calls one of the above functions on the numbers.
-function operate()
+function operate(firstNum, secondNum, operator)
