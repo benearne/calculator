5d16e1c8d1053a768daca7dceceb8188734fa89b
benearne <b.arnold@posteo.de>
2025-04-20 14:54:53 +0200

    create files, add basic stuff

---
+++ script.js
@@ -0,0 +1,13 @@
+// Create three variables, one for each part of the operation. You’ll use these variables to update your display later.
+let firstNum
+let secondNum
+let operator
+
+// four function
+function add()
+function subtract()
+function multiply()
+function divide()
+
+// Create a new function operate that takes an operator and two numbers and then calls one of the above functions on the numbers.
+function operate()
